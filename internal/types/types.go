8746ef82f58854e67c59072d54bc0bb16914a01d
yurchenko.s <stas66691@yandex.ru>
2022-05-16 14:23:08 +0300

    increment 7 fix tests

--- internal/types/types.go
+++ internal/types/types.go
@@ -34,9 +34,9 @@ type ServerConfig struct {
 }
 
 func (c *AgentConfig) Parse() error {
-	flag.StringVar(&c.Address, "a", c.Address, "http address to send metrics in format localhost:8080")
-	flag.DurationVar(&c.ReportInterval, "r", c.ReportInterval, "interval to send metrics to server. Inactive for server.")
-	flag.DurationVar(&c.PollInterval, "p", c.PollInterval, "Interval to collect metrics. Inactive for server.")
+	flag.StringVar(&c.Address, "a", "localhost:8080", "http address to send metrics in format localhost:8080")
+	flag.DurationVar(&c.ReportInterval, "r", 10*time.Second, "interval to send metrics to server. Inactive for server.")
+	flag.DurationVar(&c.PollInterval, "p", 2*time.Second, "Interval to collect metrics. Inactive for server.")
 	flag.Parse()
 
 	err := env.Parse(c)
