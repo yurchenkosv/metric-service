d66033c79ef30d236ddb66d7694457f71c4d08f3
yurchenko.s <stas66691@yandex.ru>
2022-05-16 14:20:48 +0300

    increment 7 fix tests

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,8 +1,8 @@
 package types
 
 import (
+	"flag"
 	"github.com/caarlos0/env/v6"
-	flag "github.com/spf13/pflag"
 	"time"
 )
 
@@ -34,9 +34,9 @@ type ServerConfig struct {
 }
 
 func (c *AgentConfig) Parse() error {
-	flag.StringVarP(&c.Address, "address", "a", c.Address, "http address to send metrics in format localhost:8080")
-	flag.DurationVarP(&c.ReportInterval, "report", "r", c.ReportInterval, "interval to send metrics to server. Inactive for server.")
-	flag.DurationVarP(&c.PollInterval, "poll", "p", c.PollInterval, "Interval to collect metrics. Inactive for server.")
+	flag.StringVar(&c.Address, "a", c.Address, "http address to send metrics in format localhost:8080")
+	flag.DurationVar(&c.ReportInterval, "r", c.ReportInterval, "interval to send metrics to server. Inactive for server.")
+	flag.DurationVar(&c.PollInterval, "p", c.PollInterval, "Interval to collect metrics. Inactive for server.")
 	flag.Parse()
 
 	err := env.Parse(c)
@@ -44,10 +44,10 @@ func (c *AgentConfig) Parse() error {
 }
 
 func (c *ServerConfig) Parse() error {
-	flag.StringVarP(&c.Address, "address", "a", "localhost:8080", "http address in format localhost:8080")
-	flag.DurationVarP(&c.StoreInterval, "interval", "i", 300*time.Second, "when to flush metrics to disk. Inactive for agent.")
-	flag.StringVarP(&c.StoreFile, "filepath", "f", "/tmp/devops-metrics-db.json", "path to file where metrics are stored. Inactive for agent.")
-	flag.BoolVarP(&c.Restore, "restore", "r", true, "If set to true, read file in -f flag to restore metrics state")
+	flag.StringVar(&c.Address, "a", "localhost:8080", "http address in format localhost:8080")
+	flag.DurationVar(&c.StoreInterval, "i", 300*time.Second, "when to flush metrics to disk. Inactive for agent.")
+	flag.StringVar(&c.StoreFile, "f", "/tmp/devops-metrics-db.json", "path to file where metrics are stored. Inactive for agent.")
+	flag.BoolVar(&c.Restore, "r", true, "If set to true, read file in -f flag to restore metrics state")
 	flag.Parse()
 
 	err := env.Parse(c)
