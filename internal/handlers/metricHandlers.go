9e9b8703fc71597e7cc58ca9935ea4c70b23155b
yurchenko.s <stas66691@yandex.ru>
2022-09-08 20:39:49 +0300

    fix

--- internal/handlers/metricHandlers.go
+++ internal/handlers/metricHandlers.go
@@ -202,9 +202,9 @@ func (h MetricHandler) HandleGetMetricJSON(writer http.ResponseWriter, request *
 	log.Warnf("found metric: %v", *foundMetric)
 	switch foundMetric.MType {
 	case "counter":
-		msg = fmt.Sprintf("%s:counter:%s", foundMetric.ID, foundMetric.Delta.String())
+		msg = fmt.Sprintf("%s:counter:%s", metric.ID, metric.Delta.String())
 	case "gauge":
-		msg = fmt.Sprintf("%s:gauge:%s", foundMetric.ID, foundMetric.Value.String())
+		msg = fmt.Sprintf("%s:gauge:%s", metric.ID, metric.Value.String())
 	}
 
 	foundMetric.Hash, err = h.metricService.CreateSignedHash(msg)
