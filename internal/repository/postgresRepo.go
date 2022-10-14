95fc7ba209b08d3f8dada686945e718d031917f0
yurchenko.s <stas66691@yandex.ru>
2022-10-14 10:09:24 +0400

    Fix tests

--- internal/repository/postgresRepo.go
+++ internal/repository/postgresRepo.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	_ "database/sql"
+
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
