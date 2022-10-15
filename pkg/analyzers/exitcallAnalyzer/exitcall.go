c19281ad4762f6b6b117de35f66a84f1dc79056b
yurchenkosv <stas66691@yandex.ru>
2022-10-15 22:18:46 +0300

    add build time variables (#24)
    * add build time variables
    
    * Add tests, fix program logic
    
    * Fix go vet
    
    * Fix go vet
    
    * Fix tests
    
    * Fix tests
    
    * rollback

--- pkg/analyzers/exitcallAnalyzer/exitcall.go
+++ pkg/analyzers/exitcallAnalyzer/exitcall.go
@@ -6,14 +6,16 @@ import (
 	"golang.org/x/tools/go/analysis"
 )
 
-const Doc = ` check that no os.Exit call made from main package`
+const Doc = `check that no os.Exit call made from main package`
 
 var Analyzer = &analysis.Analyzer{
-	Name: "noExit",
-	Doc:  Doc,
-	Run:  run,
+	Name: "noExit", // Analyzer name
+	Doc:  Doc,      // Documentation of analyzer
+	Run:  run,      // Main function that analyzer executes
 }
 
+// run is main function analyzer executes
+// This function searches for os.Exit in main package
 func run(pass *analysis.Pass) (interface{}, error) {
 
 	for _, file := range pass.Files {
@@ -30,6 +32,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	return nil, nil
 }
 
+// findExitFunctionCall is helper function that searches for Exit call in ast tree
 func findExitFunctionCall(pass *analysis.Pass, x *ast.CallExpr) {
 	switch exp := x.Fun.(type) {
 	case *ast.SelectorExpr:
